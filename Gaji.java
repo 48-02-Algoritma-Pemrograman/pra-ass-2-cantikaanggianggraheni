f5fbf682367ac2e0c3711e7c4e17a7f094cbfbc8
cantikaanggianggraheni <cantikaanggianggraheni@gmail.com>
2024-12-08 17:22:25 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,60 +1,47 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Gaji{
     public static int masaKerja;
-    public static void main(String[] args){
 
-        /* Pekerjaan anda mulai dari sini */
-        int HitungGaji;
-        int HitungLembur;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Masukkan masa kerja:");
-        masaKerja = input.nextInt();
-        System.out.println("Masukkan jam lembur:");
-        int jamLembur = input.nextInt();
-
-        HitungGaji = hitungGaji();
-
-        HitungLembur = hitungLembur(jamLembur);
-
-        int totalGaji = HitungGaji + HitungLembur;
-        System.out.println("Total Gaji :"+ totalGaji);
+    public static int gaji(int masaKerja){
+    if (masaKerja<5) {
+        return 5000000;
+    } else if (masaKerja>=5&&masaKerja<=10) {
+        return 7500000;
+    } else 
+    return 10000000;
     }
 
+    public static int lembur(int masaKerja){
+        if (masaKerja<5) {
+            return 40000;
+        } else if (masaKerja>=5&&masaKerja<=10) {
+            return 50000;
+        } else 
+        return 75000;
+    }
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        masaKerja = input.nextInt();
+        int jamLembur = input.nextInt();
+
         /* Pekerjaan anda mulai dari sini */
-    public static int hitungGaji(){
-        int gajiPokok;
-            if (masaKerja < 5){
-                gajiPokok = 5000000;
-            }
-            else if (masaKerja >= 5 && masaKerja <= 10) {
-                gajiPokok = 7500000;
-                
-            }
-            else{
-                gajiPokok = 10000000;
-            }
-            return  gajiPokok;
-        } 
 
-    public static int hitungLembur(int jamLembur){
-        int Uanglembur;
-            if (masaKerja < 5){
-                Uanglembur = jamLembur * 40000;
-            }
-            else if (masaKerja >= 5 && masaKerja <= 10) {
-                Uanglembur = jamLembur * 50000;
-                
-            }
-            else{
-                Uanglembur = jamLembur * 75000;
-            }
-            return Uanglembur;
-        } 
+        int gajiBulanan = gaji(masaKerja);
+        int lemburBulanan = lembur(masaKerja);
 
+        lemburBulanan*=jamLembur;
+        gajiBulanan+=lemburBulanan;
 
+        System.out.println(gajiBulanan);
+        
         /* Pekerjaan anda berakhir sini */
 
-    
+        System.out.println(Gaji.class.getDeclaredMethods().length);
+        System.out.println(Gaji.class.getDeclaredFields().length);
+        
+        input.close();
+    }
     
 }
